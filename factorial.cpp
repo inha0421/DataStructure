28e3ed400d7a7415840e4f6752922b7c58960c49
inha0421 <inha0421@gmail.com>
2016-01-05 20:39:34 +0900

    test

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial (int n){
+	if (n<=1)
+		return (1);
+	
+	else
+		return n*factorial(n-1);	
+		
+}
+
+int main (void){
+
+	int n=0;
+
+	scanf("%d",&n);
+	printf("%d! = %d\n", n, factorial(n));
+
+	return 0;
+}
