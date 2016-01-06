65eeba7817a9f7a34417948760a72ef5ecd5fcfb
inha0421 <inha0421@gmail.com>
2016-01-06 22:18:41 +0900

    headda

--- struct.c
+++ struct.c
@@ -4,13 +4,14 @@
 struct point {
 	int x;
 	int y;
+	struct point *next;
 };
 
 int main (void){
-	struct point *p1 = (struct point *p1) malloc (sizeof (struct point));
+	struct point *p1 = (struct point*) malloc (sizeof (struct point));
+	struct point *p2= (struct point*) malloc (sizeof (struct point));
 	p1->x=1;
 	p1->y=2;
-
 
  	printf("%d %d\n",p1->x, p1->y);
 	return 0;
