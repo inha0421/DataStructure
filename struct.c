e5a716c90de207e7e8806d993882f15e654ff30d
inha0421 <inha0421@gmail.com>
2016-01-06 22:23:17 +0900

    headdagosudo git add -A!

--- struct.c
+++ struct.c
@@ -12,7 +12,13 @@ int main (void){
 	struct point *p2= (struct point*) malloc (sizeof (struct point));
 	p1->x=1;
 	p1->y=2;
+	p2->x=1;
+	p2->y=2;
 
- 	printf("%d %d\n",p1->x, p1->y);
+	p1-> next = p2;
+	p2-> next = p1;
+
+ 	printf("%d %d\n",p1->next->x, p1->next->y);
+ 	printf("%d %d\n",p2->next->x, p2->next->y);
 	return 0;
 }
